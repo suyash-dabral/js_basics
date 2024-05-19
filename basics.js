5f1057878d7a35738f89c817c7d2abdf80f135a3
Suyash Dabral <suyashdabral05@gmail.com>
2024-05-19 20:22:20 +0530

    starting of javascript

---
+++ basics.js
@@ -0,0 +1,19 @@
+const id=12124
+let email="gmail"
+var pass="12334"
+city= "Dun"
+
+//const cannot be changed
+// id= 123123  // Not allowed
+
+/*   To print values               */
+
+console.log(id)
+console.log(email)
+console.log(pass)
+console.log(city)
+
+//Using tabular struct ure
+console.table([id,email,pass,city])
+
+//
