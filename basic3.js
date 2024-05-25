fcf274d5da1cd4ddaa0826e9fdcb2d7b2dccd2cc
Suyash Dabral <suyashdabral05@gmail.com>
2024-05-25 09:00:12 +0530

    update

--- basic3.js
+++ basic3.js
@@ -5,4 +5,4 @@ const rep=80
 console.log(name+rep+"yo");     //earlier syntax(outdated)
 
 
-console.log('Yo my name is ${name} and my age is ${rep}');
+console.log(`Yo my name is ${name} and my age is ${rep}`);
